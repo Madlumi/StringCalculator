106173c42457cbb754b7c4bfecf942fa24b0eb5f
Madlumi <61489225+Madlumi@users.noreply.github.com>
2022-03-16 02:06:25 +0200

    added tests

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,9 +1,19 @@
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StringCalculatorTest {
+  StringCalculator sc= new StringCalculator();
+
   @Test
-  public void testAdd(){
-    assertEquals(1,1);
+  public void testAddNone(){
+    assertEquals(0,sc.add(""));
+  }
+  @Test
+  public void testAddOne(){
+    assertEquals(1, sc.add("1"));
+  }
+  public void testAddTwo(){
+    assertEquals(3,sc.add("1,2"));
   }
 }
