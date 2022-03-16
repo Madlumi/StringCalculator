5b52a04e2ca56c91a9733b1e38751bae2bdab00f
Madlumi <61489225+Madlumi@users.noreply.github.com>
2022-03-16 02:02:37 +0200

    dummy test

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -2,4 +2,8 @@ public class StringCalculator {
   public static void main(String args[]){
     System.out.println("test");
   }
+
+  public int add(String numbers){
+    return 0;
+  }
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,2 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class StringCalculatorTest {
+  @Test
+  public void testAdd(){
+    assertEquals(1,1);
+  }
 }
