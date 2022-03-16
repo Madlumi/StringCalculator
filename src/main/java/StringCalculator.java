5c4e5e3b446267207d9abbe456b1df310544faa3
Madlumi <61489225+Madlumi@users.noreply.github.com>
2022-03-16 02:31:14 +0200

    refactored and added some more tests

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -4,13 +4,15 @@ public class StringCalculator {
   }
 
   public int add(String numbers){
-    System.out.println(numbers.length());
     int sum = 0;
+    //loop though all the numbers
     for(int i = 0; i < numbers.length();){
-      int n = numbers.substring(i).indexOf(',');
-      if(n>0){
-        sum+=Integer.parseInt(numbers.substring(i,i+n));
-        i += n+1;
+      //get length until next separator
+      int untilNextSepparator = numbers.substring(i).indexOf(',');
+      //check if there is a next separator, then add the number
+      if(untilNextSepparator>0){
+        sum+=Integer.parseInt(numbers.substring(i,i+untilNextSepparator));
+        i += untilNextSepparator+1;
       }else{
         sum+=Integer.parseInt(numbers.substring(i));
         break;
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -17,4 +17,12 @@ public class StringCalculatorTest {
   public void testAddTwo(){
     assertEquals(3,sc.add("1,2"));
   }
+  @Test
+  public void testAddMisc(){
+    assertEquals(100,sc.add("99,1"));
+    assertEquals(0,sc.add("0,0"));
+    assertEquals(0,sc.add("-100,100"));
+    assertEquals(-100,sc.add("-100"));
+    assertEquals(0,sc.add("0"));
+  }
 }
