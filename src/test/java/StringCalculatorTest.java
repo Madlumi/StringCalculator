750264344ddf24b6dcf981659888e34a86ee6a9a
Madlumi <61489225+Madlumi@users.noreply.github.com>
2022-03-24 10:43:51 +0200

    testing moving intitest inside functions

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -19,7 +19,7 @@ public class StringCalculatorTest {
   PrintStream ps = new PrintStream(stream);
 
    // System.setOut(ops);
-  @BeforeEach
+  // @BeforeEach
   public void initTests(){
     ByteArrayOutputStream stream = new ByteArrayOutputStream();
     PrintStream ps = new PrintStream(stream);
@@ -34,7 +34,7 @@ public class StringCalculatorTest {
 
   @Test
   public void testWelcome()throws Exception {
-
+    initTests();
     ByteArrayInputStream in = new ByteArrayInputStream("\n".getBytes());
     System.setIn(in);
     System.setOut(ps);
@@ -52,6 +52,7 @@ public class StringCalculatorTest {
   }
   @Test
   public void testInput()throws Exception {
+    initTests();
     ByteArrayInputStream in = new ByteArrayInputStream("scalc '1,2,3'\n\n".getBytes());
     System.setIn(in);
     System.setOut(ps);
@@ -70,6 +71,7 @@ public class StringCalculatorTest {
 
   @Test
   public void testContInput()throws Exception {
+    initTests();
     ByteArrayInputStream in = new ByteArrayInputStream("scalc '1,2,3'\nscalc '1,2,3'\nscalc '1,2,3'\n\n".getBytes());
     System.setIn(in);
     System.setOut(ps);
@@ -93,47 +95,57 @@ public class StringCalculatorTest {
 
   @Test
   public void testExtraDelims()throws Exception {
+    initTests();
     assertEquals(7,sc.add("//[***][qqq]\n1***2qqq4"));
   }
   @Test
   public void testExtraDelimsOne()throws Exception {
+    initTests();
     assertEquals(7,sc.add("//**\n1**2,4"));
   }
   @Test
   public void testExtraDelimsOneB()throws Exception {
+    initTests();
     assertEquals(7,sc.add("//[***]\n1***2,4"));
   }
   @Test
   public void testAddNone()throws Exception {
+    initTests();
     StringCalculator.Logger ml = mock(StringCalculator.Logger.class);
     StringCalculator sc= new StringCalculator(ml);
     assertEquals(0,sc.add(""));
   }
   @Test
   public void testAddOne()throws Exception {
+    initTests();
     assertEquals(1, sc.add("1"));
   }
   @Test
   public void testAddTwo()throws Exception {
+    initTests();
     assertEquals(3,sc.add("1,2"));
   }
   @Test
   public void testMany()throws Exception {
+    initTests();
     assertEquals(15,sc.add("1,2,4,8"));
   }
   @Test
   public void testNewRow() throws Exception {
+    initTests();
     assertEquals(3,sc.add("1\n2"));
   }
 
   @Test
   public void testNewRowAndComa()throws Exception {
+    initTests();
     assertEquals(7,sc.add("1\n2,4"));
   }
 
 
   @Test
   public void testAddMiscNumbers() throws Exception {
+    initTests();
     assertEquals(100,sc.add("99,1"));
     assertEquals(0,sc.add("0,0"));
     assertEquals(0,sc.add("0"));
@@ -141,39 +153,45 @@ public class StringCalculatorTest {
 
   @Test
   public void testCustomSeps() throws Exception {
+    initTests();
     assertEquals(7,sc.add("//;\n1;2,4"));
   }
   @Test
   public void testNeg(){
+    initTests();
     Throwable e = assertThrows(Exception.class, ()->{sc.add("-1");} );
     assertTrue(e.getMessage().contains("Negatives not allowed"));
   }
 
   @Test
   public void testTooBig()  throws Exception{
+    initTests();
     assertEquals(1001,sc.add("1001"));
     verify(ml,times(1)).log(1001);
   }
   @Test
   public void testTooBig2()  throws Exception{
+    initTests();
     assertEquals(2002,sc.add("1001,1001"));
     verify(ml,times(2)).log(1001);
   }
   @Test
   public void testTooBig3()  throws Exception{
+    initTests();
     assertEquals(2001,sc.add("1001,1000"));
     verify(ml,times(1)).log(1001);
     verify(ml,times(0)).log(1000);
   }
   @Test
   public void testNotTooBig()  throws Exception{
-
+    initTests();
     assertEquals(1000,sc.add("1000"));
     verify(ml,times(0)).log(1000);
 
   }
   @Test
   public void testNotTooBig2()  throws Exception{
+    initTests();
     assertEquals(2000,sc.add("1000,1000"));
     verify(ml,times(0)).log(1000);
 
